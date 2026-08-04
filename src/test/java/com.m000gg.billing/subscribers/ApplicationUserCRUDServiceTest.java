26276708ce1a0a778f96284845c1800fb3a84fc0
Vladyslav Livandovskyi <nonameb281@gmail.com>
2026-08-04 13:19:45 +0200

    (#18) fixed test import

--- src/test/java/com.m000gg.billing/subscribers/ApplicationUserCRUDServiceTest.java
+++ src/test/java/com.m000gg.billing/subscribers/ApplicationUserCRUDServiceTest.java
@@ -10,7 +10,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
